c46a465b4d94fa3f982e1631f1cf6cf919ac514e
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-30 08:15:57 -0800

    Task 1 : coreection

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Regex for mutliple chars in between string
 if ARGV.length != 0
-    if ARGV[0] =~ /hbt{2,} n/
+    if ARGV[0] =~ /hbt{2,5}n/
       puts ARGV[0]
     end
 else
