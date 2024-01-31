2826a1145e595b4f84737b5237300b4d92dc83f7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-30 20:06:55 -0800

    Task 3: Repetition, one or more

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# Regex for one or more in between string
+if ARGV.length != 0
+    if ARGV[0] =~ /hbt{1,}n/
+      puts ARGV[0]
+    end
+else
+  puts "Please provide argument (atlest 1)"
+end
