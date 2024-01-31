edb086d95af13453a9ab944f6f037680d8b9df72
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-30 20:12:12 -0800

    Task 4: Repetition, excluding one occurence

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# Regex for one or more in between string
+# Excluding one occurence - hbon 
+if ARGV.length != 0
+    if ARGV[0] =~ /hbt{0,}n/
+      puts ARGV[0]
+    end
+else
+  puts "Please provide argument (atlest 1)"
+end
