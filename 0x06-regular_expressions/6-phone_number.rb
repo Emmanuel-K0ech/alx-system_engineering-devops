cd71cf55b18ee5cc829fcc8adbf57900c8c5ed05
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-30 20:59:05 -0800

    Task 6: correction

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -2,7 +2,7 @@
 #Regex for a 10digit phone number
 #with a look ahead for spaces
 if ARGV.length != 0
-  if ARGV[0] =~ /^(?!\s)[0-9]{10}/
+  if ARGV[0] =~ /^(?!\s)[0-9]{10}(?<!\s)$/
       puts ARGV[0]
   end
 else
