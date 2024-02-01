398d111cbffbe14386dab3d407d4b164b47c5e9f
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-02-01 07:46:13 -0800

    making file executabel

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,7 +2,7 @@
 # Regex for one or more in between string
 # Excluding one occurence - hbon 
 if ARGV.length != 0
-    if ARGV[0] =~ /hbt{0,}n/
+    if ARGV[0] =~ /hbt*n/
       puts ARGV[0]
     end
 else
