5a508772b59f85e92e4ba0d1848ae866dec4f721
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-02-02 04:35:02 -0800

    repetition

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,9 +1,3 @@
 #!/usr/bin/env ruby
 # Regex for mutliple chars in between string
-if ARGV.length != 0
-  if ARGV[0] =~ /hbt?n/
-      puts ARGV[0]
-    end
-else
-  puts "Please provide argument (atlest 1)"
-end
+puts ARGV[0].scan(/hb?tn/).join
