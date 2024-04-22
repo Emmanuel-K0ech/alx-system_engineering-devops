cc3cd6e05015369e2c0c60950fd9698ab263d234
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-22 17:29:26 +0300

    spelling error of requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,5 +7,5 @@ package { 'python3-pip':
 
 # Install flask using pip3
 exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
+    command => '/usr/bin/pip3 install flask==2.1.0',
 }
