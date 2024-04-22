81af6684e6ccb0b6186c8faf26f19f0961e210f8
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-22 17:43:21 +0300

    code refactor

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # This script installs flask from pip3
 
-# Ensure pip3 is installed
-package { 'python3-pip':
-    ensure => present,
-}
-
-# Install flask using pip3
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
