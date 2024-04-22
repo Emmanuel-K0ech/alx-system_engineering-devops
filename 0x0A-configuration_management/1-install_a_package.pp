560d8bd9ab5b4be7cc6fd8c282d3a17c4b58f261
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-22 17:17:14 +0300

    Task 1: installl Flask script

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# This script installs flask from pip3
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+    ensure => present,
+}
+
+# Install flask using pip3
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask',
+}
