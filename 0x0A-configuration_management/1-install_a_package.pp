971458a9f9fffa5723ceb300dad8b4acc78e2ab6
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-22 17:19:21 +0300

    Adding version requirement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,5 +7,5 @@ package { 'python3-pip':
 
 # Install flask using pip3
 exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask',
+    command => '/usr/bin/pip3 install Flask==2.1.0',
 }
