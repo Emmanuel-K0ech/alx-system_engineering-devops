fa882d8b317d4195b53b8976df9c08e5f69ca9b0
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-11 22:34:23 +0300

    Task 1 : flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Ensure pip3 is installed and manage the package
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Ensure the specific version of Flask is installed using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
 }
