931857820009945608cbde01873ea1fbc97f68d5
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-30 02:42:33 +0300

    Task 0: Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Returns infomation about and employees ToDo list """
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <emloyee_id>".format(sys.argv[0]))
+        sys.exit(1)
+    # Base url for the JSONPlaceholder API
+    base_url - "https://jsonplaceholder.typicode.com/"
+    user = request.get(base_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    # Extracts the titles of completed tasks
+    completed = [x.get("title") for x in todos if x.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
