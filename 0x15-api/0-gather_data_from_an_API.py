cf22155e1d3d176a2c9aff4ca292e47cf6c76282
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-30 02:45:56 +0300

    Task 0: Make file executable

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Returns infomation about and employees ToDo list """
+import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -7,9 +9,9 @@ if __name__ == "__main__":
         print("Usage: {} <emloyee_id>".format(sys.argv[0]))
         sys.exit(1)
     # Base url for the JSONPlaceholder API
-    base_url - "https://jsonplaceholder.typicode.com/"
-    user = request.get(base_url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
     # Extracts the titles of completed tasks
     completed = [x.get("title") for x in todos if x.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
