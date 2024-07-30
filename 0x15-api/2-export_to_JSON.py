73a7506fdd130447a36b6075bb7bc7e38767cdf1
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-30 03:53:45 +0300

    Task 2: Export to JSON - updated codebase

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     user = requests.get(base_url).json()
     base_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         uid)
-    todos = requests.get(base_url).json()
+    todo = requests.get(base_url).json()
     name = user.get('username')
-    x = [{"task": x.get("title"), "username": x.get("completed")}
-         for x in todos]
-    ftext = {}
-    ftext[uid] = x
-    with open("{}.json".format(uid), 'w')as filejs:
-        json.dump(ftext, filejs)
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    jb = {}
+    jb[uid] = t
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(jb, filejs)
