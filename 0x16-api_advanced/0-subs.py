f67c014a722d50e8d258d71d7ee214c00041050b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-08-15 06:14:31 +0300

    Task 0: Pycodestyle - Line too long, sorted

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     """Return total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Helpful_Bread_7117)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+        (by /u/Helpful_Bread_7117)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
