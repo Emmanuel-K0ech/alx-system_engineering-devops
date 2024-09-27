99bc4dff09eec321e44de7e148f61276035e2d02
General-Manu <kipchumbakoech1107@gmail.com>
2024-09-27 21:56:56 +0300

    Task Advanced: File formatting

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Puppet code to increase file limits of user
-exec { 'file limit config':
+exec { 'file-limit config':
   command => "sed -i s/'nofile 5'/'nofile 100'/g /etc/security/limits.conf",
   path    => '/bin'
 }
 
-exec { 'file limit config_2':
+exec { 'file-limit config_2':
   command => "sed -i s/'nofile 4'/'nofile 100'/g /etc/security/limits.conf",
   path    => '/bin'
 }
